f8b6ec4c246f6e5e3fbbf3c7d432c203f8e5ec71
Alfred E. Heggestad <aeh@db.org>
2011-03-11 11:32:46 +0000

    patch: add hash_valid_size()

--- src/hash/hash.c
+++ src/hash/hash.c
@@ -181,3 +181,20 @@ void hash_flush(struct hash *h)
 	for (i=0; i<h->bsize; i++)
 		list_flush(&h->bucket[i]);
 }
+
+
+/**
+ * Calculate a valid hash size from a random size
+ *
+ * @param size Requested size
+ *
+ * @return Valid hash size
+ */
+uint32_t hash_valid_size(uint32_t size)
+{
+	uint32_t x;
+
+	for (x=0; (uint32_t)1<<x < size && x < 31; x++);
+
+	return 1<<x;
+}
