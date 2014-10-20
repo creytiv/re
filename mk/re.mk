a1c0e8c896428a19c9b9e67de8c1c3af2e68e2b2
Richard Aas <richard@db.org>
2014-10-20 12:50:44 +0000

    mk: recognize machine i686-w64-mingw32

--- mk/re.mk
+++ mk/re.mk
@@ -202,7 +202,8 @@ OS        := $(shell uname -s | sed -e s/SunOS/solaris/ | tr "[A-Z]" "[a-z]")
 endif
 
 
-ifneq ($(strip $(filter i386-mingw32 i486-mingw32 i586-mingw32msvc mingw32, \
+ifneq ($(strip $(filter i386-mingw32 i486-mingw32 i586-mingw32msvc \
+	i686-w64-mingw32 mingw32, \
 	$(MACHINE))),)
 	OS   := win32
 ifeq ($(MACHINE), mingw32)
