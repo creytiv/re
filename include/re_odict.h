b026ae2920565c2401e918afa40044589c89b879
Richard Aas <richard@db.org>
2015-09-30 21:30:10 +0000

    odict: declare enum outside struct

--- include/re_odict.h
+++ include/re_odict.h
@@ -4,6 +4,16 @@
  * Copyright (C) 2010 - 2015 Creytiv.com
  */
 
+enum odict_type {
+	ODICT_OBJECT,
+	ODICT_ARRAY,
+	ODICT_STRING,
+	ODICT_INT,
+	ODICT_DOUBLE,
+	ODICT_BOOL,
+	ODICT_NULL,
+};
+
 struct odict {
 	struct list lst;
 	struct hash *ht;
@@ -19,15 +29,7 @@ struct odict_entry {
 		double dbl;            /* ODICT_DOUBLE */
 		bool boolean;          /* ODICT_BOOL   */
 	} u;
-	enum odict_type {
-		ODICT_OBJECT,
-		ODICT_ARRAY,
-		ODICT_STRING,
-		ODICT_INT,
-		ODICT_DOUBLE,
-		ODICT_BOOL,
-		ODICT_NULL,
-	} type;
+	enum odict_type type;
 };
 
 int odict_alloc(struct odict **op, uint32_t hash_size);
