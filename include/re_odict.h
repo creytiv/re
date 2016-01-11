3e92dfa139f478fcaabcef9cc2eeb6628ab0ff69
Richard Aas <richard@db.org>
2016-01-11 06:43:45 +0000

    odict: fix typo

--- include/re_odict.h
+++ include/re_odict.h
@@ -23,7 +23,7 @@ struct odict_entry {
 	struct le le, he;
 	char *key;
 	union {
-		struct odict *odict;   /* ODICT_OBJECT / OJECT_ARRAY */
+		struct odict *odict;   /* ODICT_OBJECT / ODICT_ARRAY */
 		char *str;             /* ODICT_STRING */
 		int64_t integer;       /* ODICT_INT    */
 		double dbl;            /* ODICT_DOUBLE */
