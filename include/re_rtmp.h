044dabe3f5433c2f3469b4f69aa6be7c49b46295
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2018-09-21 11:42:40 +0200

    api cleanup

--- include/re_rtmp.h
+++ include/re_rtmp.h
@@ -9,7 +9,6 @@ enum {
 	RTMP_PORT = 1935,
 };
 
-
 /* Stream IDs */
 enum {
 
@@ -17,7 +16,6 @@ enum {
 	   (known as the control stream) */
 	RTMP_CONTROL_STREAM_ID = 0
 };
-
 
 enum rtmp_packet_type {
 	RTMP_TYPE_SET_CHUNK_SIZE     = 1,   /* Set Chunk Size               */
@@ -51,12 +49,6 @@ enum rtmp_event_type {
 	RTMP_EVENT_PING_RESPONSE      = 7,
 };
 
-/* forward declarations */
-struct sa;
-struct odict;
-struct tcp_sock;
-
-
 struct rtmp_header {
 	unsigned format:2;           /* type 0-3 */
 	uint32_t chunk_id;           /* from 3-65599 */
@@ -71,6 +63,81 @@ struct rtmp_header {
 struct rtmp_amf_message {
 	struct odict *dict;
 };
+
+/* forward declarations */
+struct sa;
+struct odict;
+struct tcp_sock;
+
+
+/* AMF Message */
+
+bool rtmp_amf_message_get_number(const struct rtmp_amf_message *msg,
+				 uint64_t *num, unsigned ix);
+const char *rtmp_amf_message_string(const struct rtmp_amf_message *msg,
+				    unsigned ix);
+
+
+/*
+ * RTMP High-level API (socket, connection, stream)
+ */
+
+
+struct rtmp_conn;
+
+typedef void (rtmp_estab_h)(void *arg);
+typedef void (rtmp_command_h)(struct rtmp_amf_message *msg, void *arg);
+typedef void (rtmp_status_h)(const struct rtmp_amf_message *msg, void *arg);
+typedef void (rtmp_close_h)(int err, void *arg);
+
+
+int rtmp_connect(struct rtmp_conn **connp, const char *uri,
+		 rtmp_estab_h *estabh, rtmp_status_h *statush,
+		 rtmp_close_h *closeh, void *arg);
+int rtmp_accept(struct rtmp_conn **connp, struct tcp_sock *ts,
+		rtmp_estab_h *estabh, rtmp_command_h *cmdh,
+		rtmp_status_h *statush, rtmp_close_h *closeh, void *arg);
+uint32_t rtmp_window_ack_size(const struct rtmp_conn *conn);
+struct tcp_conn *rtmp_conn_tcpconn(const struct rtmp_conn *conn);
+int rtmp_control(struct rtmp_conn *conn, enum rtmp_packet_type type, ...);
+int rtmp_amf_reply(struct rtmp_conn *conn, const struct rtmp_amf_message *req,
+		   unsigned body_propc, ...);
+int rtmp_conn_debug(struct re_printf *pf, const struct rtmp_conn *conn);
+
+
+struct rtmp_stream;
+
+typedef void (rtmp_ready_h)(void *arg);
+typedef void (rtmp_audio_h)(uint32_t timestamp,
+			    const uint8_t *pld, size_t len, void *arg);
+typedef void (rtmp_video_h)(uint32_t timestamp,
+			    const uint8_t *pld, size_t len, void *arg);
+
+int  rtmp_play(struct rtmp_stream **streamp, struct rtmp_conn *conn,
+	       const char *name, rtmp_ready_h *readyh,
+	       rtmp_audio_h *auh, rtmp_video_h *vidh, void *arg);
+int  rtmp_publish(struct rtmp_stream **streamp, struct rtmp_conn *conn,
+		  const char *name, rtmp_ready_h *ready, void *arg);
+int  rtmp_send_audio(struct rtmp_stream *strm, uint32_t timestamp,
+		     const uint8_t *pld, size_t len);
+int  rtmp_send_video(struct rtmp_stream *strm, uint32_t timestamp,
+		     const uint8_t *pld, size_t len);
+bool rtmp_stream_isready(const struct rtmp_stream *strm);
+int  rtmp_stream_debug(struct re_printf *pf, const struct rtmp_stream *strm);
+struct rtmp_stream *rtmp_stream_alloc(struct rtmp_conn *conn,
+				      const char *name,
+				      uint32_t stream_id,
+				      rtmp_ready_h *readyh,
+				      rtmp_audio_h *auh,
+				      rtmp_video_h *vidh,
+				      void *arg);
+
+
+#if 1
+/*
+ * XXX: This is the low level API, will be removed after code is stable
+ */
+
 
 /*
  * RTMP Header
@@ -113,76 +180,5 @@ int rtmp_amf_encode_object(struct mbuf *mb, enum rtmp_amf_type container,
 
 int rtmp_amf_decode(struct rtmp_amf_message **msgp, struct mbuf *mb);
 
-/* AMF Message */
 
-bool rtmp_amf_message_get_number(const struct rtmp_amf_message *msg,
-				 uint64_t *num, unsigned ix);
-const char *rtmp_amf_message_string(const struct rtmp_amf_message *msg,
-				    unsigned ix);
-
-
-/*
- * RTMP High-level API (socket, connection, stream)
- */
-
-
-struct rtmp_conn;
-
-typedef void (rtmp_estab_h)(void *arg);
-typedef void (rtmp_command_h)(struct rtmp_amf_message *msg, void *arg);
-typedef void (rtmp_status_h)(const struct rtmp_amf_message *msg, void *arg);
-typedef void (rtmp_close_h)(int err, void *arg);
-
-
-int rtmp_connect(struct rtmp_conn **connp, const char *uri,
-		 rtmp_estab_h *estabh, rtmp_status_h *statush,
-		 rtmp_close_h *closeh, void *arg);
-int rtmp_accept(struct rtmp_conn **connp, struct tcp_sock *ts,
-		rtmp_estab_h *estabh, rtmp_command_h *cmdh,
-		rtmp_status_h *statush, rtmp_close_h *closeh, void *arg);
-uint32_t rtmp_window_ack_size(const struct rtmp_conn *conn);
-struct tcp_conn *rtmp_conn_tcpconn(const struct rtmp_conn *conn);
-int rtmp_conn_debug(struct re_printf *pf, const struct rtmp_conn *conn);
-
-
-struct rtmp_stream;
-
-typedef void (rtmp_ready_h)(void *arg);
-typedef void (rtmp_audio_h)(uint32_t timestamp,
-			    const uint8_t *pld, size_t len, void *arg);
-typedef void (rtmp_video_h)(uint32_t timestamp,
-			    const uint8_t *pld, size_t len, void *arg);
-
-int  rtmp_play(struct rtmp_stream **streamp, struct rtmp_conn *conn,
-	       const char *name, rtmp_ready_h *readyh,
-	       rtmp_audio_h *auh, rtmp_video_h *vidh, void *arg);
-int  rtmp_publish(struct rtmp_stream **streamp, struct rtmp_conn *conn,
-		  const char *name, rtmp_ready_h *ready, void *arg);
-int  rtmp_send_audio(struct rtmp_stream *strm, uint32_t timestamp,
-		     const uint8_t *pld, size_t len);
-int  rtmp_send_video(struct rtmp_stream *strm, uint32_t timestamp,
-		     const uint8_t *pld, size_t len);
-bool rtmp_stream_isready(const struct rtmp_stream *strm);
-int  rtmp_stream_debug(struct re_printf *pf, const struct rtmp_stream *strm);
-struct rtmp_stream *rtmp_stream_alloc(struct rtmp_conn *conn,
-				      const char *name,
-				      uint32_t stream_id,
-				      rtmp_ready_h *readyh,
-				      rtmp_audio_h *auh,
-				      rtmp_video_h *vidh,
-				      void *arg);
-
-
-/*
- * Server
- */
-
-int rtmp_amf_reply(struct rtmp_conn *conn, const struct rtmp_amf_message *req,
-		   unsigned body_propc, ...);
-
-
-/*
- * Control
- */
-
-int rtmp_control(struct rtmp_conn *conn, enum rtmp_packet_type type, ...);
+#endif
