5db62cc06a2e6872a792b54e3a911419828b1f79
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2018-10-12 13:19:04 +0200

    rtmp: code review

--- src/rtmp/conn.c
+++ src/rtmp/conn.c
@@ -474,26 +474,21 @@ static void connect_resp_handler(bool success, const struct odict *msg,
 
 static int send_connect(struct rtmp_conn *conn)
 {
-	const int aucodecs  = 0x0400;  /* AAC  */
-	const int vidcodes  = 0x0080;  /* H264 */
-	int err;
+	const int ac  = 0x0400;  /* AAC  */
+	const int vc  = 0x0080;  /* H264 */
 
-	err = rtmp_amf_request(conn, RTMP_CONTROL_STREAM_ID, "connect",
-			       connect_resp_handler, conn,
-			       1,
-		       RTMP_AMF_TYPE_OBJECT, 8,
-		         RTMP_AMF_TYPE_STRING, "app", conn->app,
-		         RTMP_AMF_TYPE_STRING, "flashVer", "LNX 9,0,124,2",
-		         RTMP_AMF_TYPE_STRING, "tcUrl", conn->uri,
-		         RTMP_AMF_TYPE_BOOLEAN, "fpad", false,
-		         RTMP_AMF_TYPE_NUMBER, "capabilities", 15.0,
-		         RTMP_AMF_TYPE_NUMBER, "audioCodecs", (double)aucodecs,
-		         RTMP_AMF_TYPE_NUMBER, "videoCodecs", (double)vidcodes,
-		         RTMP_AMF_TYPE_NUMBER, "videoFunction", 1.0);
-	if (err)
-		return err;
-
-	return 0;
+	return rtmp_amf_request(conn, RTMP_CONTROL_STREAM_ID, "connect",
+				connect_resp_handler, conn,
+				1,
+			RTMP_AMF_TYPE_OBJECT, 8,
+		          RTMP_AMF_TYPE_STRING, "app", conn->app,
+		          RTMP_AMF_TYPE_STRING, "flashVer", "LNX 9,0,124,2",
+		          RTMP_AMF_TYPE_STRING, "tcUrl", conn->uri,
+		          RTMP_AMF_TYPE_BOOLEAN, "fpad", false,
+		          RTMP_AMF_TYPE_NUMBER, "capabilities", 15.0,
+		          RTMP_AMF_TYPE_NUMBER, "audioCodecs", (double)ac,
+		          RTMP_AMF_TYPE_NUMBER, "videoCodecs", (double)vc,
+		          RTMP_AMF_TYPE_NUMBER, "videoFunction", 1.0);
 }
 
 
