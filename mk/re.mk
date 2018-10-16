7394ee407adceb6a984e1b2bac515c8afad46ed9
Alfred E. Heggestad <alfred.heggestad@gmail.com>
2018-10-16 08:57:31 +0200

    mk: add version numbers to compiler flags (#162)

--- mk/re.mk
+++ mk/re.mk
@@ -610,6 +610,11 @@ CCACHE	:= $(shell [ -e /usr/bin/ccache ] 2>/dev/null \
 	|| [ -e /opt/local/bin/ccache ] \
 	&& echo "ccache")
 CFLAGS  += -DVERSION=\"$(VERSION)\"
+CFLAGS  += \
+	-DVER_MAJOR=$(VER_MAJOR) \
+	-DVER_MINOR=$(VER_MINOR) \
+	-DVER_PATCH=$(VER_PATCH)
+
 
 # Enable gcov Coverage testing
 #
