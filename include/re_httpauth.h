c6a84aaf9507706c514e8051bf05fcf1ac71f5f5
Christian Spielberger <c.spielberger@commend.com>
2019-04-09 17:05:15 +0200

    httpauth: Add basic authentication for HTTP clients
    This commit adds functions httpauth_basic_decode, httpauth_basic_make_response
    and httpauth_basic_encode in order to generate a Basic Authorization header for
    the HTTP client.

--- include/re_httpauth.h
+++ include/re_httpauth.h
@@ -34,6 +34,14 @@ struct httpauth_digest_resp {
 };
 
 
+/** HTTP Basic */
+struct httpauth_basic {
+	struct mbuf *mb;
+	struct pl realm;
+	struct pl auth;
+};
+
+
 int httpauth_digest_challenge_decode(struct httpauth_digest_chall *chall,
 				     const struct pl *hval);
 int httpauth_digest_response_decode(struct httpauth_digest_resp *resp,
@@ -46,3 +54,10 @@ int httpauth_digest_make_response(struct httpauth_digest_resp **resp,
 		const char *pwd, const char *body);
 int httpauth_digest_response_encode(const struct httpauth_digest_resp *resp,
 				  struct mbuf *mb);
+
+struct httpauth_basic *httpauth_basic_alloc(void);
+int httpauth_basic_decode(struct httpauth_basic *basic,
+		const struct pl *hval);
+int httpauth_basic_make_response(struct httpauth_basic *basic,
+		const char *user, const char *pwd);
+int httpauth_basic_encode(const struct httpauth_basic *basic, struct mbuf *mb);
